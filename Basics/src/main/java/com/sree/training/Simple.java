1ba98ecbb0968719ee667c83c2f29137322c4bc7
sreenivasraom <sreenivasrao.m@lemtom.com>
2014-04-17 12:45:08 +0530

    Adding Basics module

---
+++ Basics/src/main/java/com/sree/training/Simple.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.sree.training;
+
+/**
+ * @author sreenivasrao.m
+ *
+ */
+class Simple {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Java");
+
+	}
+
+}
